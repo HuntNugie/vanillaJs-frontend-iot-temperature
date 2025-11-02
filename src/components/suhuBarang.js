7526509a4ebe408f1c2dd5e41ce2f4a8915829ee
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-02 23:56:41 +0700

    page:memberikan data fetch kepada component

--- src/components/suhuBarang.js
+++ src/components/suhuBarang.js
@@ -1,8 +1,8 @@
-export const suhuBarang = () => {
+export const suhuBarang = (data) => {
   return `  <!-- Suhu Barang Dekat -->
       <div id="card-object" class="bg-gray-800 rounded-2xl p-5 shadow-xl hover:shadow-2xl transition">
         <h2 class="text-lg font-semibold text-gray-300 mb-2">Suhu Barang Dekat</h2>
-        <div class="text-4xl font-bold text-orange-400" id="object-temp">32°C</div>
+        <div class="text-4xl font-bold text-orange-400" id="object-temp">${data}°C</div>
         <p class="text-sm text-gray-400 mt-2">Sensor mendeteksi suhu tinggi</p>
       </div>
 `;
